dbda1b1dc822291e53c84803a53755234c4c1e7d
Maksim Karalevich <m@karalevich.com>
2021-07-06 14:05:03 -0700

    initial

---
+++ public/code.js
@@ -0,0 +1,75 @@
+'use strict';
+
+figma.showUI(__html__, { width: 232 });
+figma.ui.onmessage = (msg) => {
+    if (msg.type === "destroy-components") {
+        const selection = [...figma.currentPage.selection];
+        // let instances: SceneNode[] = [];
+        // let components: SceneNode[] = [];
+        function destroyComps(nodes) {
+            if (Array.isArray(nodes)) {
+                return nodes.map((child) => {
+                    if (child.type === "INSTANCE" && !child.removed) {
+                        console.log("detached");
+                        child.detachInstance();
+                        // if (detached.children) destroyComps(detached.children);
+                    }
+                    // If Component
+                    // if (child.type === "COMPONENT" && !child.removed) {
+                    // 	const parent = figma.currentPage.findOne(
+                    // 		(n) => n.id === child.parentId
+                    // 	);
+                    // 	const copy = child.createInstance();
+                    // 	copy.x = child.x;
+                    // 	copy.y = child.y;
+                    // 	console.log(parent);
+                    // }
+                });
+            }
+        }
+        destroyComps(selection);
+    }
+    if (msg.type === "unstyle-elements") {
+        const selection = [...figma.currentPage.selection];
+        const clearStyles = (node) => {
+            if (node.type === "TEXT") {
+                node.textStyleId = "";
+                node.effectStyleId = "";
+                node.fillStyleId = "";
+                node.strokeStyleId = "";
+            }
+            if (node.type === "FRAME") {
+                node.gridStyleId = "";
+                node.effectStyleId = "";
+                node.fillStyleId = "";
+                node.strokeStyleId = "";
+            }
+            if (node.type === "RECTANGLE" ||
+                node.type === "ELLIPSE" ||
+                node.type === "LINE" ||
+                node.type === "STAR" ||
+                node.type === "VECTOR" ||
+                node.type === "POLYGON" ||
+                node.type === "BOOLEAN_OPERATION") {
+                node.effectStyleId = "";
+                node.fillStyleId = "";
+                node.strokeStyleId = "";
+            }
+            if (node.type === "GROUP" ||
+                node.type === "INSTANCE" ||
+                node.type === "COMPONENT")
+                return null;
+        };
+        function removeStyles(nodes) {
+            if (Array.isArray(nodes)) {
+                return nodes.map((child) => {
+                    clearStyles(child);
+                    if (child.children)
+                        removeStyles(child.children);
+                });
+            }
+        }
+        removeStyles(selection);
+    }
+    // figma.closePlugin();
+};
---
+++ src/PluginUI.svelte
@@ -0,0 +1,47 @@
+<script>
+	import { GlobalCSS } from 'figma-plugin-ds-svelte';
+
+	import { Button, Input, Label, SelectMenu } from 'figma-plugin-ds-svelte';
+
+	let pattern = ""
+
+	function destroyComps() {
+		parent.postMessage({
+			pluginMessage: {
+				'type': 'destroy-components',
+				'pattern': pattern
+			}
+		}, '*');
+	}
+	function unstyleEls() {
+		parent.postMessage({
+			pluginMessage: {
+				'type': 'unstyle-elements',
+			}
+		}, '*');
+	}
+
+	function cancel() {
+		parent.postMessage({ pluginMessage: { 'type': 'cancel' } }, '*')
+	}
+
+</script>
+
+
+<div class="wrapper p-xxsmall">
+
+	<Label>Exclude pattern</Label>
+	<Input bind:value={pattern} class="mb-xxsmall" />
+
+	<div class="flex p-xxsmall mb-xsmall">
+		<Button on:click={cancel} variant="secondary" class="mr-xsmall">Cancel</Button>
+		<Button on:click={destroyComps} class="mr-xsmall">Destroy</Button>
+		<Button on:click={unstyleEls}>Unstyle</Button>
+	</div>
+
+</div>
+
+
+<style>
+
+</style>
