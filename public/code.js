ef533af424edb196cdced9eb66962278b1466fd9
Maksim Karalevich <m@karalevich.com>
2021-08-10 11:27:00 -0700

    feat: add notifications

--- public/code.js
+++ public/code.js
@@ -4,6 +4,8 @@ figma.showUI(__html__, { width: 320, height: 96 });
 figma.ui.onmessage = (msg) => {
     if (msg.type === "destroy-components") {
         const selection = [...figma.currentPage.selection];
+        if (selection.length === 0)
+            figma.notify(`Please select layers first`);
         let updatedSelection = [];
         function destroyComps(nodes) {
             if (Array.isArray(nodes)) {
@@ -22,12 +24,30 @@ figma.ui.onmessage = (msg) => {
             }
         }
         destroyComps(selection);
+        const message = () => {
+            const instances = updatedSelection.length;
+            if (instances === 0)
+                return `Nothing to destroy here`;
+            if (instances === 1)
+                return `❏  ${instances} instance was detached`;
+            if (instances >= 1)
+                return `❏  ${instances} instances were detached`;
+        };
+        figma.notify(message());
         figma.currentPage.selection = updatedSelection;
     }
     if (msg.type === "unstyle-elements") {
         const selection = [...figma.currentPage.selection];
+        if (selection.length === 0)
+            figma.notify(`Please select layers first`);
+        let acc = [];
         const clearStyles = (node) => {
             if (node.type === "TEXT") {
+                if (node.textStyleId !== "" ||
+                    node.effectStyleId !== "" ||
+                    node.fillStyleId !== "" ||
+                    node.strokeStyleId !== "")
+                    acc = [...acc, node];
                 node.textStyleId = "";
                 node.effectStyleId = "";
                 node.fillStyleId = "";
@@ -36,6 +56,11 @@ figma.ui.onmessage = (msg) => {
             if (node.type === "FRAME" ||
                 node.type === "INSTANCE" ||
                 node.type === "COMPONENT") {
+                if (node.gridStyleId !== "" ||
+                    node.effectStyleId !== "" ||
+                    node.fillStyleId !== "" ||
+                    node.strokeStyleId !== "")
+                    acc = [...acc, node];
                 node.gridStyleId = "";
                 node.effectStyleId = "";
                 node.fillStyleId = "";
@@ -48,6 +73,10 @@ figma.ui.onmessage = (msg) => {
                 node.type === "VECTOR" ||
                 node.type === "POLYGON" ||
                 node.type === "BOOLEAN_OPERATION") {
+                if (node.effectStyleId !== "" ||
+                    node.fillStyleId !== "" ||
+                    node.strokeStyleId !== "")
+                    acc = [...acc, node];
                 node.effectStyleId = "";
                 node.fillStyleId = "";
                 node.strokeStyleId = "";
@@ -65,6 +94,16 @@ figma.ui.onmessage = (msg) => {
             }
         }
         removeStyles(selection);
+        const message = () => {
+            const layers = acc.length;
+            if (layers === 0)
+                return `Nothing to destroy here`;
+            if (layers === 1)
+                return `❏  ${layers} layer was unstyled`;
+            if (layers >= 1)
+                return `❏  ${layers} layers were unstyled`;
+        };
+        figma.notify(message());
     }
     // figma.closePlugin();
 };
--- src/code.ts
+++ src/code.ts
@@ -3,6 +3,7 @@ figma.showUI(__html__, { width: 320, height: 96 });
 figma.ui.onmessage = (msg) => {
 	if (msg.type === "destroy-components") {
 		const selection = [...figma.currentPage.selection];
+		if (selection.length === 0) figma.notify(`Please select layers first`);
 		let updatedSelection = [];
 
 		function destroyComps(nodes) {
@@ -20,14 +21,30 @@ figma.ui.onmessage = (msg) => {
 			}
 		}
 		destroyComps(selection);
+		const message = () => {
+			const instances = updatedSelection.length;
+			if (instances === 0) return `Nothing to destroy here`;
+			if (instances === 1) return `❏  ${instances} instance was detached`;
+			if (instances >= 1) return `❏  ${instances} instances were detached`;
+		};
+		figma.notify(message());
 		figma.currentPage.selection = updatedSelection;
 	}
 
 	if (msg.type === "unstyle-elements") {
 		const selection = [...figma.currentPage.selection];
+		if (selection.length === 0) figma.notify(`Please select layers first`);
+		let acc = [];
 
 		const clearStyles = (node) => {
 			if (node.type === "TEXT") {
+				if (
+					node.textStyleId !== "" ||
+					node.effectStyleId !== "" ||
+					node.fillStyleId !== "" ||
+					node.strokeStyleId !== ""
+				)
+					acc = [...acc, node];
 				node.textStyleId = "";
 				node.effectStyleId = "";
 				node.fillStyleId = "";
@@ -38,6 +55,13 @@ figma.ui.onmessage = (msg) => {
 				node.type === "INSTANCE" ||
 				node.type === "COMPONENT"
 			) {
+				if (
+					node.gridStyleId !== "" ||
+					node.effectStyleId !== "" ||
+					node.fillStyleId !== "" ||
+					node.strokeStyleId !== ""
+				)
+					acc = [...acc, node];
 				node.gridStyleId = "";
 				node.effectStyleId = "";
 				node.fillStyleId = "";
@@ -52,6 +76,12 @@ figma.ui.onmessage = (msg) => {
 				node.type === "POLYGON" ||
 				node.type === "BOOLEAN_OPERATION"
 			) {
+				if (
+					node.effectStyleId !== "" ||
+					node.fillStyleId !== "" ||
+					node.strokeStyleId !== ""
+				)
+					acc = [...acc, node];
 				node.effectStyleId = "";
 				node.fillStyleId = "";
 				node.strokeStyleId = "";
@@ -69,6 +99,13 @@ figma.ui.onmessage = (msg) => {
 			}
 		}
 		removeStyles(selection);
+		const message = () => {
+			const layers = acc.length;
+			if (layers === 0) return `Nothing to destroy here`;
+			if (layers === 1) return `❏  ${layers} layer was unstyled`;
+			if (layers >= 1) return `❏  ${layers} layers were unstyled`;
+		};
+		figma.notify(message());
 	}
 
 	// figma.closePlugin();
